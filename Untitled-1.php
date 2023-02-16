b1523ded0842c80503d5910e2b032c3988771ad1
Grec88 <Lihtinmann88@yandex.ru>
2023-02-16 11:20:51 +0300

    The page was created

---
+++ Untitled-1.php
@@ -0,0 +1,5 @@
+<?php
+echo 123;
+function fun(int $a, int $b){
+    return $a+$b;
+}
---
+++ index.php
@@ -0,0 +1,23 @@
+<html>
+
+<head>
+    <title>
+        Der mieine projekt
+    </title>
+    <meta charset="utf-8">
+    <link rel="stylesheet" src="style.css">
+</head>
+
+<body>
+    <header class="header">
+        <h1 class="header__title">
+            Du hast!
+        </h1>
+    </header>
+</body>
+
+</html>
+
+<?php
+echo "Hello, world!";
+?>
